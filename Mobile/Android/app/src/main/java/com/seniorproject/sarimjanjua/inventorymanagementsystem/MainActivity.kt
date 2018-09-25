a0c590143176ffcfa2bb154bde9107360a4bf6a9
Sarim Janjua <sjanjua94@gmail.com>
2018-09-25 17:44:24 -0400

    Added Activity for Home Page

--- Mobile/Android/app/src/main/java/com/seniorproject/sarimjanjua/inventorymanagementsystem/MainActivity.kt
+++ Mobile/Android/app/src/main/java/com/seniorproject/sarimjanjua/inventorymanagementsystem/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.seniorproject.sarimjanjua.inventorymanagementsystem
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +12,16 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById( R.id.loginButton )
+
+
+    }
+
+    fun loginButtonClicked( view : View) : Unit
+    {
+        val intent = Intent( this, HomePage::class.java )
+
+        startActivity( intent )
     }
 }
