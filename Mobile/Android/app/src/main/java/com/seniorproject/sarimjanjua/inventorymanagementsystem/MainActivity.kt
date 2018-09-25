2bf066013b2e026a04efd1f6bb9ff8d9b095e533
Sarim Janjua <sjanjua94@gmail.com>
2018-09-25 15:47:54 -0400

    Ignore

--- Mobile/Android/app/src/main/java/com/seniorproject/sarimjanjua/inventorymanagementsystem/MainActivity.kt
+++ Mobile/Android/app/src/main/java/com/seniorproject/sarimjanjua/inventorymanagementsystem/MainActivity.kt
@@ -3,11 +3,11 @@ package com.seniorproject.sarimjanjua.inventorymanagementsystem
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar( findViewById( R.id.my_toolbar ) )
     }
 }
