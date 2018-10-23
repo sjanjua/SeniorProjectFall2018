dab3e4bc651517dcde68d33890fb5f505a5eedf8
KyleParker14 <36014982+KyleParker14@users.noreply.github.com>
2018-10-23 19:34:59 -0400

    Create Users Table change
    resized some columns

--- Database/DDL/Users_Table_Script.sql
+++ Database/DDL/Users_Table_Script.sql
@@ -3,11 +3,11 @@ use inventory;
 CREATE TABLE users (
 userId char(20) NOT NULL PRIMARY KEY
 , password char(20) NOT NULL
-, first_name char(20) NOT NULL
-, last_name char(30) NOT NULL
+, first_name char(25) NOT NULL
+, last_name char(25) NOT NULL
 , phone_number char(10) NOT NULL
-, street char(40) NOT NULL
-, city char(40) NOT NULL
+, street char(25) NOT NULL
+, city char(25) NOT NULL
 , zip_code char(5) NOT NULL
 , email char(40) NOT NULL
 , user_type char(1) NOT NULL
