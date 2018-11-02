4cdc04882f9e3709b086a45c46fbaa47394684e7
KyleParker14 <parkerk2@students.rowan.edu>
2018-11-01 21:53:31 -0400

    Dont allow duplicate UserID

--- Website/Code/Inventory/Inventory/Controllers/AccountController.cs
+++ Website/Code/Inventory/Inventory/Controllers/AccountController.cs
@@ -51,13 +51,13 @@ namespace Inventory.Controllers
                     return View("Login", user);
                 }
             }
-
             return View("Login", user);
         }
 
         [HttpPost]
         public ActionResult AddUser(Users user)
         {
+            Logon logIn = null;
             if (!user.Password_Field.Equals(user.Password_Field1))
             {
                 ModelState.AddModelError("Password_Field", "The passwords entered do not match");
@@ -65,36 +65,46 @@ namespace Inventory.Controllers
             }
             else
             {
-                Logon logIn = null;
-                if (ModelState.IsValid)
+                using (MySqlConnection conn = DBUtils.GetConnection())
                 {
-                    using (MySqlConnection conn = DBUtils.GetConnection())
+                    UsersRepository repo = new UsersRepository(conn);
+                    Users isNew = repo.GetById(user.UserID);
+                    if (isNew != null)
                     {
-                        UsersRepository repo = new UsersRepository(conn);
-                        Dictionary<String, Object> hash = new Dictionary<String, Object>();
-                        hash.Add("UserID", user.UserID);
-                        hash.Add("Password_Field", user.Password_Field);
-                        hash.Add("First_Name", user.First_Name);
-                        hash.Add("Last_Name", user.Last_Name);
-                        hash.Add("Phone_Number", user.Phone_Number);
-                        hash.Add("Street", user.Street);
-                        hash.Add("City", user.City);
-                        hash.Add("Zip_Code", user.Zip_Code);
-                        hash.Add("Email", user.Email);
-                        repo.SetAll(hash);
-                        LogonRepository repo1 = new LogonRepository(conn);
-                        logIn = repo1.GetById(user.UserID);
+                        ModelState.AddModelError("UserID", "This User Name Already Exists");
+                        return View("SignUp", user);
+                    }
+                    else
+                    {
+                    if (ModelState.IsValid)
+                    {
+                        
+                            Dictionary<String, Object> hash = new Dictionary<String, Object>();
+                            hash.Add("UserID", user.UserID);
+                            hash.Add("Password_Field", user.Password_Field);
+                            hash.Add("First_Name", user.First_Name);
+                            hash.Add("Last_Name", user.Last_Name);
+                            hash.Add("Phone_Number", user.Phone_Number);
+                            hash.Add("Street", user.Street);
+                            hash.Add("City", user.City);
+                            hash.Add("Zip_Code", user.Zip_Code);
+                            hash.Add("Email", user.Email);
+                            repo.SetAll(hash);
+                            LogonRepository repo1 = new LogonRepository(conn);
+                            logIn = repo1.GetById(user.UserID);
+                        }
+                    }
+                    if (logIn != null)
+                    {
+                        return View("Login", logIn);
+                    }
+                    else
+                    {
+                        return View("SignUp");
                     }
                 }
-                if (logIn != null)
-                {
-                    return View("Login", logIn);
-                }
-                else
-                {
-                    return View("SignUp");
-                }
             }
+            return RedirectToAction("Error");
         }
 
         public ActionResult Logout()
