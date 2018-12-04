0b1393558f5a1af66466f8fb00221e5d9bd01b31
QuentinTerry <43620876+QuentinTerry@users.noreply.github.com>
2018-12-03 20:04:56 -0500

    Create Dashboard and Dashboard Repository classes

---
+++ Website/Code/Inventory/Inventory/Server/Repository/DashboardRepository.cs
@@ -0,0 +1,25 @@
+﻿using Inventory.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.DataLayer.Repository
+{
+    public class DashboardRepository : MySQLRepository<Dashboard>
+    {
+        public DashboardRepository(MySqlConnection connection) : base(connection)
+        {
+        }
+
+        public IEnumerable<Dashboard> GetAll()
+        {
+            using (var command = new MySqlCommand("SELECT * FROM customer"))
+            {
+                return GetRecords(command);
+            }
+        }
+
+    }
+}
