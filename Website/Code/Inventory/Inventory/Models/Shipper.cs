9b07949fa12f2ab5765fd4754d6c89d431635185
QuentinTerry <43620876+QuentinTerry@users.noreply.github.com>
2018-12-04 20:19:33 -0500

    Create CreateShipper method

--- Website/Code/Inventory/Inventory/Models/Shipper.cs
+++ Website/Code/Inventory/Inventory/Models/Shipper.cs
@@ -27,4 +27,20 @@ namespace Inventory.Models
         [DisplayName("Shipper")]
         public string ShipperName { get; set; }
     }
+
+    public class CreateShipper
+    {
+        [ScaffoldColumn(false)]
+        [Display(Name = "Shipper"), Required(ErrorMessage = "Please select SHipper")]
+        public int ShipperID { get; set; }
+        [Display(Name = "Shipper Name"), DataType(DataType.Date), Required(ErrorMessage = "Please enter Order Date")]
+        public DateTime ShipperName { get; set; }
+        [Display(Name = "Phone No"), DataType(DataType.Date), Required(ErrorMessage = "Please enter Required Date")]
+        public DateTime Phone { get; set; }
+        [Display(Name = "Freight")]
+        public decimal Freight { get; set; }
+
+        public List<DisplayCustomer> CustomerList { get; set; }
+        public List<DisplayShipper> ShipperList { get; set; }
+    }
 }
