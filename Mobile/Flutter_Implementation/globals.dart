e5835cfad60bc94b56694be729bdeac3a3b9c82a
Turnern4 <33550085+Turnern4@users.noreply.github.com>
2018-12-18 03:07:42 -0500

    light comments

--- Mobile/Flutter_Implementation/globals.dart
+++ Mobile/Flutter_Implementation/globals.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+//Setting background color, bar color and the style of text for Globals 
 class Globals
 {
   static Color     backgroundColor = Color.fromRGBO( 58, 66, 86, 1.0 );
   static Color     barColor        = Color.fromRGBO( 37, 42, 56, 1.0 );
   static TextStyle textStyle       = TextStyle( fontFamily: 'Ubuntu' );
-}
+}//END OF CLASS
