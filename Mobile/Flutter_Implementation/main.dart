afd1f395401bea3fa3fb4519a9a1ef210ecf746a
Turnern4 <33550085+Turnern4@users.noreply.github.com>
2018-12-19 22:55:56 -0500

    small comments

--- Mobile/Flutter_Implementation/main.dart
+++ Mobile/Flutter_Implementation/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 
+//**************************************************
+//Main method of the app 
+//Starts the login screen as soon as the app loads 
+//**************************************************
 void main()
 {
   runApp( logInScreen() );
